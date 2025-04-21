a33a6b29cdeaad7f2fe0c51ba6d60fb09a1e215f
maresama <s1F102500179@iniad.org>
2025-04-21 15:19:38 +0900

    一気に裏返す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,40 @@ export default function Home() {
 
     if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
       newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
+      for (let i = 1; i < 8; i++) {
+        if (board[y + i] !== undefined && board[y + i][x] === 3 - turnColor) {
+          newBoard[y + i][x] = turnColor;
+        }
+      }
     }
+
+    if (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      for (let i = 1; i < 8; i++) {
+        if (board[x + i] !== undefined && board[y][x + i] === 3 - turnColor) {
+          newBoard[y][x + i] = turnColor;
+        }
+      }
+    }
+    if (board[x - 1] !== undefined && board[y][x - 1] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      for (let i = 1; i < 8; i++) {
+        if (board[x - i] !== undefined && board[y][x - i] === 3 - turnColor) {
+          newBoard[y][x - i] = turnColor;
+        }
+      }
+    }
+
+    if (board[y - 1] !== undefined && board[y - 1][x] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      for (let i = 1; i < 8; i++) {
+        if (board[y - i] !== undefined && board[y - 1][x] === 3 - turnColor) {
+          newBoard[y - i][x] = turnColor;
+        }
+      }
+    }
+
+    setTurnColor(3 - turnColor);
     setBoard(newBoard);
   };
   return (
